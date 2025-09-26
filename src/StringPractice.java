3ca8cc645e22e83d64b37477738c39bfabd16bf1
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-09-25 21:05:26 -0700

    string init & length

--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,8 +1,10 @@
 public class StringPractice {
   public static void main(String[] args) {
     // Create a string with at least 5 characters and assign it to a variable
+    String fiveCharacterString = "Tests";
 
     // Find the length of the string
+    System.out.println(fiveCharacterString.length());
 
     // Concatenate (add) two strings together and reassign the result
 
