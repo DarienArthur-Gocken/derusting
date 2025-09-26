a485bb2fb4f0b247ca87aa8e2ac5acb4be0aca56
Darien Arthur-Gocken <233949874+DarienArthur-Gocken@users.noreply.github.com>
2025-09-25 21:28:13 -0700

    hashset contains, remove, size

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -12,10 +12,13 @@ public class SetPractice {
     hashStrings.add("test3");
 
     // Check whether the Set contains a given String
+    System.out.println(hashStrings.contains("test1"));
 
     // Remove an element from the Set
+    hashStrings.remove("test3");
 
     // Get the size of the Set
+    System.out.println(hashStrings.size());
 
     // Iterate over the elements of the Set, printing each one on a separate line
 
